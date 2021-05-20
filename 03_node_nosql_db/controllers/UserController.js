813f321afb28f10264d37134adec03d72f9055e3
89P13 <fc.armando@outlook.com>
2021-05-20 17:02:38 -0500

    Agregar endpoint y logica para encontrar usuario por id

--- 03_node_nosql_db/controllers/UserController.js
+++ 03_node_nosql_db/controllers/UserController.js
@@ -26,4 +26,14 @@ module.exports = {
         res.status(400).json({ message: 'error fetching users', error });
         }
     },
+    findOne: async (req, res) => {
+        const id = req.params.idUser;
+
+        try{
+          const user = await User.findById(id);
+          return res.status(200).json({ message: 'user found', user })
+        } catch (error) {
+          return res.status(500).json({ error });
+        }
+    },
 }
--- 03_node_nosql_db/routers/UserRouter.js
+++ 03_node_nosql_db/routers/UserRouter.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 const UserContoller = require('../controllers/UserController');
 
+//Creat
 router.post('/users', UserContoller.create);
 
+//Read (all)
 router.get('/users', UserContoller.findAll);
+
+//Read one
+router.get('/users/:idUser', UserContoller.findOne);
+//Update one
+
+//Delete (Logical, One)
 
 module.exports = router;
